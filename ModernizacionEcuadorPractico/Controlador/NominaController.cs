1792286d1d3d21c32957faa626686a06d27ebf55
Julissa Chamaidan <matheomathias1513-sketch@gmail.com>
2026-06-01 01:49:32 -0500

    feat: Integración de Modelo, Controlador y Vista para el cálculo de nómina

---
+++ ModernizacionEcuadorPractico/Controlador/NominaController.cs
@@ -0,0 +1,39 @@
+﻿using ModernizacionEcuadorPractico.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernizacionEcuadorPractico.Controlador
+{
+    public class NominaController
+    {
+        // Simulamos una base de datos en memoria con una lista
+        private List<Empleado> _empleados;
+        private int _contadorId = 1;
+
+        public NominaController()
+        {
+            _empleados = new List<Empleado>();
+        }
+
+        public void RegistrarEmpleado(string nombre, decimal salarioBase, int horasExtras)
+        {
+            var nuevoEmpleado = new Empleado
+            {
+                Id = _contadorId++,
+                Nombre = nombre,
+                SalarioBase = salarioBase,
+                HorasExtras = horasExtras
+            };
+
+            _empleados.Add(nuevoEmpleado);
+        }
+
+        public List<Empleado> ObtenerTodos()
+        {
+            return _empleados.ToList();
+        }
+    }
+}
---
+++ ModernizacionEcuadorPractico/Entidades/Empleado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernizacionEcuadorPractico.Entidades
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal SalarioBase { get; set; }
+        public int HorasExtras { get; set; }
+
+        // Propiedad calculada: El salario base + $15 por cada hora extra
+        public decimal SalarioTotal
+        {
+            get { return SalarioBase + (HorasExtras * 15); }
+        }
+    }
+}
